c7fffc11ac438c70fc9c8c092e0890017db8afd0
Brian Manley <brian.manley91@gmail.com>
2023-04-08 11:31:41 -0400

    Added hello world endpoint

--- src/main/kotlin/dev/manley/kotlinapitemplate/infrastructure/web/controllers/HelloWorldController.kt
+++ src/main/kotlin/dev/manley/kotlinapitemplate/infrastructure/web/controllers/HelloWorldController.kt
@@ -1,4 +1,15 @@
-package dev.manley.kotlinapitemplate.infrastructure.web.controller
+package dev.manley.kotlinapitemplate.infrastructure.web.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/hello")
 class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello, World!"
+    }
 }

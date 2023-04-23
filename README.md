411ec4570b6cb6c2e73eae259d98c45769e0a832
Brian Manley <brian@manley.dev>
2023-04-23 19:02:00 -0400

    Added readme and swagger doc (#11)
    * Add swagger dependency
    
    * Got swagger docs working
    
    * Why was the readme in the git ignore?
    
    * Added info about swagger doc to readme
    
    * Removed extra from readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kotlin API Template
+A template rest API written in Kotlin following Clean Architecture. This repo is meant to be forked so that a "real"
+service can be built.
+
+This repo was initialized with Spring Initializr
+
+## API Documentation
+When running locally, Swagger API documentation is available at [http://localhost:8080/swagger-ui/index.html](http://localhost:8080/swagger-ui/index.html)
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:3.0.5")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.12.4")
